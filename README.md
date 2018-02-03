3313a57adc7fe956b59af523b2f2ea5b1ef06049
Ammonix <joan.kuenzler@gmail.com>
2018-02-03 02:36:13 +0100

    Adding Continuous Integration

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # redux-state-observable
+
 path to redux state property as observable
---
+++ test/test.js
@@ -0,0 +1,9 @@
+'use strict';
+var expect = require('chai').expect;
+var index = require('../dist/index.js');
+describe('createObservable function test', () => {
+    it('should return Boys', () => {
+        var result = index.createObservable('Boys');
+        expect(result).to.equal('Boys');
+    });
+});
