2e8fe9f9b25e3fe4d13b062c6f22601193d3a15d
Ammonix <joan.kuenzler@gmail.com>
2018-02-04 13:27:12 +0100

    implemented

--- lib/index.ts
+++ lib/index.ts
@@ -1,15 +1,16 @@
-// import { get as getValue } from "object-path";
+import { get as getValue } from "object-path";
 import { Store, Unsubscribe } from "redux";
 import { Observable } from "rxjs/Observable";
-export function getValue$<TState>(
+export function getValue$<TState, TValue = TState>(
   store: Store<TState>,
-  // objectPath: ObjectPathGlobal.IPath = ""
-): Observable<TState> {
-  // let value: TValue = getValue<TState, TValue>(store.getState(), objectPath);
-  return new Observable(subscriber => {
-    subscriber.next(store.getState());
+  objectPath: ObjectPathGlobal.IPath = ""
+): Observable<TValue> {
+  let value: TValue = getValue<TState, TValue>(store.getState(), objectPath);
+  return new Observable(observer => {
+    observer.next(value);
     const unsubscribe: Unsubscribe = store.subscribe(() => {
-      subscriber.next(store.getState());
+      value = getValue<TState, TValue>(store.getState(), objectPath);
+      observer.next(value);
     });
     return unsubscribe;
   });
