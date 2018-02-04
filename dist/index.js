154c0130b6832fdf224a2bcbb0a4623255af1c53
Ammonix <joan.kuenzler@gmail.com>
2018-02-04 07:11:25 +0100

    getValue$ implemented

--- dist/index.js
+++ dist/index.js
@@ -1,6 +1,16 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-function createObservable(str) {
-    return str;
+var object_path_1 = require("object-path");
+var Observable_1 = require("rxjs/Observable");
+function getValue$(store, objectPath) {
+    if (objectPath === void 0) { objectPath = ""; }
+    var value = object_path_1.get(store.getState(), objectPath);
+    return new Observable_1.Observable(function (observer) {
+        observer.next(value);
+        var unsubscribe = store.subscribe(function () {
+            observer.next(value);
+        });
+        return unsubscribe;
+    });
 }
-exports.createObservable = createObservable;
+exports.getValue$ = getValue$;
---
+++ lib/index.ts
@@ -0,0 +1,19 @@
+import { get as getValue } from "object-path";
+import {
+  Store,
+  Unsubscribe,
+} from "redux";
+import { Observable } from "rxjs/Observable";
+export function getValue$<TState, TValue = TState>(
+  store: Store<TState>,
+  objectPath: ObjectPathGlobal.IPath = ""
+): Observable<TValue> {
+  let value: TValue = getValue<TState, TValue>(store.getState(), objectPath);
+  return new Observable(observer => {
+    observer.next(value);
+    const unsubscribe: Unsubscribe = store.subscribe(() => {
+      observer.next(value);
+    });
+    return unsubscribe;
+  });
+}
