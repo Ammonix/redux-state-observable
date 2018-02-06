89b624555e20da8c83ace3c6b70e046c9f6377a2
Joan Künzler <joan.kuenzler@raiffeisen.ch>
2018-02-06 10:03:11 +0100

    docs and performance improvements

--- lib/index.ts
+++ lib/index.ts
@@ -1,16 +1,25 @@
-import { get as getValue } from "object-path";
+import { get as getValueByPath } from "object-path";
 import { Store, Unsubscribe } from "redux";
 import { Observable } from "rxjs/Observable";
+
+/**
+ * Creates an rxjs/Observable from redux store state property using object-path.
+ * 
+ * @export 
+ * @template TState 
+ * @template TValue 
+ * @param {Store<TState>} store The redux store which gets subscribed to.
+ * @param {ObjectPathGlobal.IPath} [objectPath=[]] Optional parameter which determines the path to the property that the observable emits, default is root.
+ * @returns {Observable<TValue>} The observable which emits the property each time the redux store subscription listener gets called.
+ */
 export function getValue$<TState, TValue = TState>(
   store: Store<TState>,
-  objectPath: ObjectPathGlobal.IPath = ""
+  objectPath: ObjectPathGlobal.IPath = []
 ): Observable<TValue> {
-  let value: TValue = getValue<TState, TValue>(store.getState(), objectPath);
-  return new Observable(observer => {
-    observer.next(value);
+  return new Observable(subscriber => {
     const unsubscribe: Unsubscribe = store.subscribe(() => {
-      value = getValue<TState, TValue>(store.getState(), objectPath);
-      observer.next(value);
+      const value = getValueByPath<TState, TValue>(store.getState(), objectPath);
+      subscriber.next(value);
     });
     return unsubscribe;
   });
